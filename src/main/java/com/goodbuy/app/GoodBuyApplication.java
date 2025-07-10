8081f70c0f68a792a11e53d4a8f847b588f92dd8
Eduardo Castro <eduardocastrobritoo@gmail.com>
2025-07-10 13:48:23 -0300

    ïnit

---
+++ src/main/java/com/goodbuy/app/GoodBuyApplication.java
@@ -0,0 +1,13 @@
+package com.goodbuy.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodBuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodBuyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goodbuy/app/GoodBuyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goodbuy.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodBuyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
